ea3bb422e2cd5e3774f2a01edc4887f167a04ed7
Silvi Restiani <silvirestiani0@gmail.com>
2026-08-10 07:58:41 +0700

    Update array multidimensi.php

--- php_dasar_pertemuan_4_silvi/array multidimensi.php
+++ php_dasar_pertemuan_4_silvi/array multidimensi.php
@@ -5,7 +5,6 @@ $orang = [
     ["Nama" => "lili", "Umur" => 30],
     ["Nama" => "lulu", "Umur" => 35]
 ];
-
 // Mengakses elemen array multidimensi
 echo $orang[0]["Nama"] . " berumur " . $orang[0]["Umur"] . " tahun.<br>"; // Output: lala berumur 25 tahun.
 echo $orang[1]["Nama"] . " berumur " . $orang[1]["Umur"] . " tahun.<br>"; // Output: lili berumur 30 tahun.
